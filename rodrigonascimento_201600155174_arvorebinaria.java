ddd921d6fd3fa824cbc35b391397f25b6744bc99
RodrigoFNascimento <rodrigofn0@hotmail.com>
2019-07-06 16:38:55 -0300

    Add print to file

--- rodrigonascimento_201600155174_arvorebinaria.java
+++ rodrigonascimento_201600155174_arvorebinaria.java
@@ -1,6 +1,11 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 class Arquivo {
     public String name;
@@ -45,60 +50,60 @@ public class rodrigonascimento_201600155174_arvorebinaria {
     private static Arquivo tree = null;
 
     /**
-     * Prints every node in the tree in the order:
-     * left, root, right.
+     * Prints every node in the tree in the order: left, root, right.
      * @param node Root of the tree.
+     * @throws FileNotFoundException
      */
-    private static void printEPD(Arquivo node) {
+    private static void printEPD(String fileName, Arquivo node) throws FileNotFoundException {
         if (node == null) 
             return; 
   
         /* first recur on left child */
-        printEPD(node.left); 
+        printEPD(fileName, node.left); 
   
         /* then print the data of node */
-        System.out.println(node.getOutputString() + " "); 
+        writeToFile(fileName, node.getOutputString()); 
   
         /* now recur on right child */
-        printEPD(node.right); 
+        printEPD(fileName, node.right); 
     }
 
     /**
-     * Prints every node in the tree in the order:
-     * root, left, right.
+     * Prints every node in the tree in the order: root, left, right.
      * @param node Root of the tree.
+     * @throws FileNotFoundException
      */
-    private static void printPED(Arquivo node) {
+    private static void printPED(String fileName, Arquivo node) throws FileNotFoundException {
         if (node == null) 
             return; 
   
         /* first print data of node */
-        System.out.println(node.getOutputString() + " "); 
+        writeToFile(fileName, node.getOutputString() + " "); 
   
         /* then recur on left sutree */
-        printPED(node.left); 
+        printPED(fileName, node.left); 
   
         /* now recur on right subtree */
-        printPED(node.right);
+        printPED(fileName, node.right);
     }
 
     /**
-     * Prints every node in the tree in the order:
-     * left, right, root.
+     * Prints every node in the tree in the order: left, right, root.
      * @param node Root of the tree.
+     * @throws FileNotFoundException
      */
-    private static void printEDP(Arquivo node) {
+    private static void printEDP(String fileName, Arquivo node) throws FileNotFoundException {
         if (node == null) 
             return; 
   
         // first recur on left subtree 
-        printEDP(node.left); 
+        printEDP(fileName, node.left); 
   
         // then recur on right subtree 
-        printEDP(node.right); 
+        printEDP(fileName, node.right); 
   
         // now deal with the node 
-        System.out.println(node.getOutputString() + " ");
+        writeToFile(fileName, node.getOutputString());
     }
 
     /**
@@ -139,20 +144,74 @@ public class rodrigonascimento_201600155174_arvorebinaria {
         return root; 
     }
 
+    /**
+     * Writes content to file.
+     * @param fileName Name of the file (with extension) to be writen
+     * @param content Content to be writen on the file
+     * @throws FileNotFoundException
+     */
+    private static void writeToFile(String fileName, String content) throws FileNotFoundException {
+
+        try(FileWriter fw = new FileWriter(fileName, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println("");
+            out.print(content);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Writes content to file. If the line to be written is the first one,
+     * doesn't add a line break before it, otherwise does.
+     * @param fileName
+     * @param content
+     * @param firstLine
+     * @throws FileNotFoundException
+     */
+    private static void writeToFile(String fileName, String content, boolean firstLine) throws FileNotFoundException {
+
+        try(FileWriter fw = new FileWriter(fileName, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            if (!firstLine)
+                out.println("");
+            out.print(content);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Empties the content of a file.
+     * @param fileName Name of the file.
+     * @throws FileNotFoundException
+     */
+    private static void emptyFile(String fileName) throws FileNotFoundException {
+        new PrintWriter(fileName).close();
+    }
+
     public static void main(String[] args) {
 
         try (FileInputStream inputStream = new FileInputStream(args[0])) {
             
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // Gets the number of files to be inserted
             int n = Integer.parseInt(reader.readLine());
 
             for (int i = 0; i < n; i++) {
                 Arquivo tempArquivo = new Arquivo();
                 String line = reader.readLine();
 
+                // Gets the indexes of the two spaces
                 int firstSpaceIndex = line.indexOf(" ");
                 int secondSpaceIndex = line.indexOf(" ", firstSpaceIndex + 1);
 
+                // Reads the file's info
                 tempArquivo.name = line.substring(0, firstSpaceIndex);
                 tempArquivo.permission = line.substring(firstSpaceIndex + 1, secondSpaceIndex);
                 tempArquivo.size = line.substring(secondSpaceIndex + 1);
@@ -161,12 +220,13 @@ public class rodrigonascimento_201600155174_arvorebinaria {
                 insert(tempArquivo);
             }
 
-            System.out.println("EPD:");
-            printEPD(tree);
-            System.out.println("PED:");
-            printPED(tree);
-            System.out.println("EDP:");
-            printEDP(tree);
+            emptyFile(args[1]);
+            writeToFile(args[1], "EPD:", true);
+            printEPD(args[1], tree);
+            writeToFile(args[1], "PED:");
+            printPED(args[1], tree);
+            writeToFile(args[1], "EDP:");
+            printEDP(args[1], tree);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
